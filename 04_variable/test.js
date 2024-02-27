cc29d17518476878f35b8e49b7f096cc86d067ac
amanchaturvedi505 <54510210+amanchaturvedi505@users.noreply.github.com>
2024-02-27 13:17:13 +0000

    var let and const

---
+++ 04_variable/test.js
@@ -0,0 +1,25 @@
+// var let and const 
+
+// var is used in old js as es5 and let and const is used in new js es6 
+
+// var is a function scoped variable 
+
+// function abcd(){
+//     for (var i=0;i<12;i++){
+//         console.log(i)
+//     }
+//     console.log(i)
+// }
+
+// abcd();
+
+// let and const are a braced scoped variable 
+
+// function abcde(){
+//     for (var i=0;i<5;i++){
+//         console.log(i)
+//     }
+//     console.log(i)
+// }
+
+// abcde();
