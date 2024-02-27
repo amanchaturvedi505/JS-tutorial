fa1f56b063ab6e0019783402b963e006de2d329c
amanchaturvedi505 <54510210+amanchaturvedi505@users.noreply.github.com>
2024-02-27 09:09:04 +0000

    Date in JS

---
+++ 02_date/test.js
@@ -0,0 +1,25 @@
+// let myDate=new Date()
+// console.log(myDate)
+
+// // in more improvised way 
+// console.log(myDate.toString())
+
+
+// console.log(myDate.toISOString())
+
+// // how we actually write the date 
+// console.log(myDate.toLocaleString())
+
+
+// // date is an object data type in js 
+
+// console.log( typeof myDate)
+
+
+// how to create date
+
+// let createdDate=new Date(2023,0,23);
+// console.log(createdDate.toDateString());
+
+let createdDate=new Date("27-03-2024");
+console.log(createdDate.toLocaleString());
